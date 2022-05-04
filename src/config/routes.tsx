fc49a79e383ef85fd6993efb42c4cf4e837a75e4
LarryZhao <larryzhao1994@outlook.com>
2022-05-04 17:16:15 +0800

    reset css

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,10 +1,26 @@
+import { RouteRecordRaw } from "vue-router";
+import { First } from "../components/welcome/First";
+import { Fourth } from "../components/welcome/Fourth";
+import { Second } from "../components/welcome/Second";
+import { Third } from "../components/welcome/Third";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from '../views/Welcome';
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: "/", component: Foo },
   {
     path: "/about",
     component: Bar,
+  },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Fourth },
+    ],
   },
 ];
---
+++ src/views/Welcome.tsx
@@ -0,0 +1,20 @@
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/mangosteen.svg'
+export const Welcome = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      <div class={s.wrapper}>
+        <header>
+            <img src={logo}  />
+            <h1>山竹记账</h1>
+        </header>
+        <main>
+          <RouterView />
+        </main>
+        <footer>footer</footer>
+      </div>
+    );
+  },
+});

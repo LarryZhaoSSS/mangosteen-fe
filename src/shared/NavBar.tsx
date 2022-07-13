6009848d6f0be3f381b41afc775eb7e9b55dbaa5
LarryZhao <larryzhao1994@outlook.com>
2022-07-13 22:07:19 +0800

    nav component

---
+++ src/shared/NavBar.tsx
@@ -0,0 +1,17 @@
+import { defineComponent } from 'vue';
+import s from './NavBar.module.scss'
+export const NavBar = defineComponent({
+    setup(props,context){
+        const {slots} = context
+        return ()=>(
+            <div class={s.navbar}>
+                <span class={s.icon_wrapper}>
+                    {slots.icon?.()}
+                </span>
+                <span class={s.title_wrapper}>
+                    {slots.default?.()}
+                </span>
+            </div>
+        )
+    }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -3,13 +3,19 @@ import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
 import { Icon } from '../shared/Icon';
+import { NavBar } from '../shared/NavBar';
 import s from './StartPage.module.scss'
 export const StartPage = defineComponent({
     props:['name'],
     setup(props,context){
         return ()=>(
             <div>
-                <menu>menu</menu>
+                <NavBar>
+                    {{
+                        default:'山竹记账',
+                        icon:<Icon name="menu" class={s.navIcon} />
+                    }}
+                </NavBar>
                 <Center class={s.pigWrapper}>
                     <Icon name="pig" class={s.pig} />
                 </Center>

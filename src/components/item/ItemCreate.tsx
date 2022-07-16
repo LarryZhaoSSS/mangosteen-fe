15f3c6d74223c676625b3d44aa614355ba865923
LarryZhao <larryzhao1994@outlook.com>
2022-07-16 22:04:27 +0800

    wip:tab component

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,16 +1,23 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 import s from './ItemCreate.module.scss'
 export const ItemCreate = defineComponent({
     setup(props,context){
+        const refKind = ref('支出')
         return ()=>(
             <MainLayout>
             {
                 {
                     title:()=>'记一笔',
                     icon:()=><Icon name="left" class={s.navIcon} />,
-                    default:()=><div>main</div>
+                    default:()=><>
+                    <Tabs v-model:selected={refKind.value} >
+                        <Tab name="支出">支出</Tab>
+                        <Tab name="收入">收入</Tab>
+                    </Tabs>
+                    </>
                 }
             }
         </MainLayout>

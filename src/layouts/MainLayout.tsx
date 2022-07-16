f5442bc1bf82e9c95df8d6d25e92be2205416281
LarryZhao <larryzhao1994@outlook.com>
2022-07-16 17:00:29 +0800

    create MainLayout component

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { defineComponent } from "vue";
+import { NavBar } from "../shared/NavBar";
+export const MainLayout = defineComponent({
+  setup(props, context) {
+    return () => (
+      <div>
+        <NavBar>
+          {{
+            default: () => context.slots.title?.(),
+            icon: () => context.slots.icon?.(),
+          }}
+        </NavBar>
+        {context.slots.default?.()}
+      </div>
+    );
+  },
+});
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, ref } from "vue";
 import { RouterLink } from "vue-router";
+import { MainLayout } from "../layouts/MainLayout";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
@@ -16,35 +17,39 @@ export const StartPage = defineComponent({
     };
     return () => (
       <div>
-        <NavBar>
+        <MainLayout>
           {{
-            default: () => "山竹记账",
+            title: () => "山竹记账",
             icon: () => (
               <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
             ),
+            default: () => (
+              <>
+                <Center class={s.pigWrapper}>
+                  <Icon name="pig" class={s.pig} />
+                </Center>
+                <div class={s.button_wrapper}>
+                  <RouterLink to="/items/create">
+                    <Button
+                      class={s.button}
+                      onClick={() => {
+                        console.log("hi");
+                      }}
+                    >
+                      开始记账
+                    </Button>
+                  </RouterLink>
+                </div>
+                <RouterLink to="/items/create">
+                  <FloatButton iconName="add" />
+                </RouterLink>
+                {refOverlayVisible.value && (
+                  <Overlay onClose={() => (refOverlayVisible.value = false)} />
+                )}
+              </>
+            ),
           }}
-        </NavBar>
-        <Center class={s.pigWrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to="/items/create">
-            <Button
-              class={s.button}
-              onClick={() => {
-                console.log("hi");
-              }}
-            >
-              开始记账
-            </Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton iconName="add" />
-        </RouterLink>
-        {refOverlayVisible.value && (
-          <Overlay onClose={() => (refOverlayVisible.value = false)} />
-        )}
+        </MainLayout>
       </div>
     );
   },

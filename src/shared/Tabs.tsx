d272f304f5ccc4e7fc643214fc6d1540a8d512fb
LarryZhao <larryzhao1994@outlook.com>
2022-07-17 15:38:58 +0800

    tab component

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -9,19 +9,19 @@ export const Tabs = defineComponent({
   },
   setup(props, context) {
     return () => {
-      const array = context.slots.default?.();
-      if (!array) {
+      const tabs = context.slots.default?.();
+      if (!tabs) {
         return () => null;
       }
-      for (let i = 0; i < array.length; i++) {
-        if (array[i].type !== Tab) {
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
           throw new Error("<Tabs> only accepts <Tab> as children");
         }
       }
       return (
         <div class={s.tabs}>
           <ol class={s.tabs_nav}>
-            {array.map((item) => (
+            {tabs.map((item) => (
               <li
                 class={item.props?.name === props.selected ? s.selected : null}
                 onClick={() => {
@@ -32,7 +32,11 @@ export const Tabs = defineComponent({
               </li>
             ))}
           </ol>
-          <div></div>
+          <div>
+              {
+                  tabs.find(item=>item.props?.name === props.selected)
+              }
+          </div>
         </div>
       );
     };

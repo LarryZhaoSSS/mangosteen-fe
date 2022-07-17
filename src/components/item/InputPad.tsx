89dd1e2a6161579336bdeac2750846ff8d5dd092
LarryZhao <larryzhao1994@outlook.com>
2022-07-17 16:43:47 +0800

    add style for inputpad component

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -72,12 +72,12 @@ export const InputPad = defineComponent({
     ];
     return () => (
       <>
-        <div>
-          <span class={s.notes}>
-            <Icon name="date" />
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon name="date" class={s.icon} />
             <span>2022-01-01</span>
           </span>
-          <div class={s.amount}>数字</div>
+          <div class={s.amount}>1000.00</div>
         </div>
         <div class={s.buttons}>
           {

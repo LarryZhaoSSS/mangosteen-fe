bb3bb2cea0c421687d6e00464be2383bf8064d83
LarryZhao <larryzhao1994@outlook.com>
2022-07-17 16:33:18 +0800

    wip:input pad component

---
+++ src/components/item/InputPad.tsx
@@ -0,0 +1,90 @@
+import { defineComponent } from "vue";
+import { Icon } from "../../shared/Icon";
+import s from "./InputPad.module.scss";
+export const InputPad = defineComponent({
+  setup(props, context) {
+    const buttons = [
+      {
+        text: "1",
+        onclick: () => {},
+      },
+      {
+        text: "2",
+        onclick: () => {},
+      },
+      {
+        text: "3",
+        onclick: () => {},
+      },
+      {
+        text: "清空",
+        onclick: () => {},
+      },
+      {
+        text: "4",
+        onclick: () => {},
+      },
+
+      {
+        text: "5",
+        onclick: () => {},
+      },
+      {
+        text: "6",
+        onclick: () => {},
+      },
+      {
+        text: "+",
+        onclick: () => {},
+      },
+      {
+        text: "7",
+        onclick: () => {},
+      },
+      {
+        text: "8",
+        onclick: () => {},
+      },
+      {
+        text: "9",
+        onclick: () => {},
+      },
+      {
+        text: "-",
+        onclick: () => {},
+      },
+      {
+        text: ".",
+        onclick: () => {},
+      },
+      {
+        text: "0",
+        onclick: () => {},
+      },
+      {
+        text: "删",
+        onclick: () => {},
+      },
+      {
+        text: "提交",
+        onclick: () => {},
+      },
+    ];
+    return () => (
+      <>
+        <div>
+          <span class={s.notes}>
+            <Icon name="date" />
+            <span>2022-01-01</span>
+          </span>
+          <div class={s.amount}>数字</div>
+        </div>
+        <div class={s.buttons}>
+          {
+              buttons.map(button=><button key={button.text} onClick={button.onclick}>{button.text}</button>)
+          }
+        </div>
+      </>
+    );
+  },
+});
--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,26 +1,30 @@
-import { defineComponent, ref } from 'vue';
-import { MainLayout } from '../../layouts/MainLayout';
-import { Icon } from '../../shared/Icon';
-import { Tab, Tabs } from '../../shared/Tabs';
-import s from './ItemCreate.module.scss'
+import { defineComponent, ref } from "vue";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
+import { InputPad } from "./InputPad";
+import s from "./ItemCreate.module.scss";
 export const ItemCreate = defineComponent({
-    setup(props,context){
-        const refKind = ref('支出')
-        return ()=>(
-            <MainLayout>
-            {
-                {
-                    title:()=>'记一笔',
-                    icon:()=><Icon name="left" class={s.navIcon} />,
-                    default:()=><>
-                    <Tabs v-model:selected={refKind.value} >
-                        <Tab name="支出">支出</Tab>
-                        <Tab name="收入">收入</Tab>
-                    </Tabs>
-                    </>
-                }
-            }
-        </MainLayout>
-        )
-    }
-})
+  setup(props, context) {
+    const refKind = ref("支出");
+    return () => (
+      <MainLayout>
+        {{
+          title: () => "记一笔",
+          icon: () => <Icon name="left" class={s.navIcon} />,
+          default: () => (
+            <>
+              <Tabs v-model:selected={refKind.value}>
+                <Tab name="支出">支出</Tab>
+                <Tab name="收入">收入</Tab>
+              </Tabs>
+              <div class={s.inputPad_wrapper}>
+                <InputPad />
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});

f2fec23ac6d8f720c64457abef16b5639007e1e2
LarryZhao <larryzhao1994@outlook.com>
2022-08-06 21:02:26 +0800

    ItemList styles

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,12 +1,20 @@
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
 import { Tabs,Tab } from '../../shared/Tabs';
+import { Time } from '../../shared/time';
 import s from './ItemList.module.scss'
 import { ItemSummary } from './ItemSummary';
 export const ItemList = defineComponent({
     setup(props,context){
-        const refSelected = ref(0)
+        const refSelected = ref('本月')
+        const time = new Time()
+        const customTime = reactive({start:new Time(),end:new Time()})
+        const timeList = [
+            {start:time.firstDayOfMonth(),end:time.lastDayOfMonth()},
+            {start:time.add(-1,'month').firstDayOfMonth(),end:time.add(-1,'month').lastDayOfMonth()},
+            {start:time.firstDayOfYear(),end:time.lastDayOfYear()}
+        ]
         return ()=>(
            <MainLayout>{
                {
@@ -14,10 +22,10 @@ export const ItemList = defineComponent({
                    icon:()=><Icon name="menu"/>,
                    default:()=>(
                        <Tabs classPrefix={'cutomTabs'} v-model:selected={refSelected.value}>
-                           <Tab name="本月"><ItemSummary/></Tab>
-                           <Tab name="上月"><ItemSummary/></Tab>
-                           <Tab name="今年"><ItemSummary/></Tab>
-                           <Tab name="自定义时间"><ItemSummary/></Tab>
+                           <Tab name="本月"><ItemSummary startDate={timeList[0].start.format()} endDate={timeList[0].end.format()} /></Tab>
+                           <Tab name="上月"><ItemSummary startDate={timeList[1].start.format()} endDate={timeList[1].end.format()} /></Tab>
+                           <Tab name="今年"><ItemSummary startDate={timeList[2].start.format()} endDate={timeList[2].end.format()} /></Tab>
+                           <Tab name="自定义时间"><ItemSummary startDate={customTime.start.format()} endDate={customTime.end.format()} /></Tab>
                        </Tabs>
                    )
                }
